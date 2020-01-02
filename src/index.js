d95ee2eeff306b226c323829ae434ddefa0b8797
ntur5 <pressntr@gmail.com>
2020-01-02 14:59:32 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import axios from 'axios'
+
+import TodoItem from "./todo-item"
+import "./styles.css"
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            todo: "",
+            todos: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:5000/todos')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    todos: data
+                })
+            })
+    }
+
+    handleChange = e => {
+        this.setState({
+            todo: e.target.value
+        })
+    }
+
+    renderTodos = () => {
+        return this.state.todos.map(item => {
+            return (
+                <TodoItem key={item.id} item={item} />
+            )
+        })
+    }
+
+    addTodo = e => {
+        e.preventDefault()
+        axios({
+            method: "POST",
+            url: "http://localhost:5000/todo",
+            headers: { "content-type": "application/json"},
+            data: {
+                title: this.state.todo, 
+                done: false
+            }
+        })
+        .then(data => {
+            this.setState({
+                todos: [...this.state.todos, data.data],
+                todo: ""
+            })
+        })
+        .catch(error => console.log("Add todo Error: ", error))
+    }
+
+    render() {
+        
+        return (
+            <div className="app">
+                <h1>ToDo List</h1>
+                <form className="add-todo" onSubmit={this.addTodo}>
+                    <input 
+                        type="text" 
+                        placeholder="Add Todo" 
+                        onChange={this.handleChange}
+                        value={this.state.todo} 
+                    />
+                    <button type="submit">Add</button>
+                </form>
+                {this.renderTodos()}
+            </div>
+        )
+    }
+}
+
+const rootElement = document.getElementById('root')
+ReactDom.render(<App />, rootElement)
